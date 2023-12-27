0fd22818ab110388fd1541c01255a9df57dd1a19
Peter <storm.sk123@gmail.com>
2023-12-27 13:56:46 +0000

    Update page.tsx

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,5 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
 import {fetchInvoiceById, fetchCustomers} from '@/app/lib/data'
 
 import { notFound } from 'next/navigation';
